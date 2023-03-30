ba6b52903b0db7ee1a44464a85921b7716a62bfb
NANCY2323 <121061914+kajukatlib@users.noreply.github.com>
2023-03-30 12:35:01 +0530

    Delete project9.js
    Signed-off-by: NANCY2323 <121061914+kajukatlib@users.noreply.github.com>

--- project9.js
+++
@@ -1,9 +0,0 @@
-let bar=document.getElementById("scroll");
-        window.addEventListener("scroll",function func(){
-            let scrollposition=window.scrollY;
-            let documentheight=document.documentElement.scrollHeight;
-            let windowheight=window.innerHeight;
-            var scrollpercentage=scrollposition/(documentheight-windowheight)*100;
-            bar.style.width=scrollpercentage+'%';
-        });
-        
